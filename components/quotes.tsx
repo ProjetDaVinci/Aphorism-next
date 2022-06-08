64298fc432c3933569b121f68f4d7d6c70654e4a
User <User@holding.ru>
2022-06-09 00:06:54 +0600

    fix: bug when deleting the local store, the array was not displayed, fixed

--- components/quotes.tsx
+++ components/quotes.tsx
@@ -1,17 +1,26 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { QuoteResponse, QuotesProps } from "../types/types";
 import { useLocalStorage } from "../utils/useLocalStorage";
 import Quote from "./UI/Quote";
 
 const Quotes: FC<QuotesProps> = ({ quotes }) => {
+  const equals = (arr1: Array<QuoteResponse>, arr2: Array<QuoteResponse>) => {
+    return (
+      arr1.length === arr2.length &&
+      arr1.every((value, index) => value === arr2[index])
+    );
+  };
+
   const [localQuotes, setQuotes] = useLocalStorage<QuoteResponse[]>(
     "quotes",
     []
   );
 
-  const [quotesState, setQuotesState] = useState<QuoteResponse[]>(
-    localQuotes === null ? quotes : localQuotes
-  );
+  useEffect(() => {
+    setQuotesState(equals(localQuotes, []) ? quotes : localQuotes);
+  }, []);
+
+  const [quotesState, setQuotesState] = useState<QuoteResponse[]>([]);
 
   const like = (quote: string, author: string, liked: boolean) => {
     quotesState.map((item) => {
